00555e2481b92366e6830373e60ebdd332015e31
Aman Manwani <aman04.manwani@gmail.com>
2025-04-14 15:14:21 +0530

    added lambda function code

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_key_pair" "ec2_kp" {
   key_name   = "ec2_key_pair"
-  public_key = file("~/.ssh/id_rsa.pub") # Path to your public key
+  public_key = file("~/.ssh/id_rsa.pub") 
 }
 
 # EC2 Security Group
@@ -50,12 +50,22 @@ resource "aws_instance" "web" {
 resource "aws_s3_bucket" "static_site" {
   bucket = var.bucket_name
 
-
   tags = {
     Name = "StaticWebsiteBucket"
   }
 }
 
+# Block public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Enable static website hosting
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.static_site.id
 
@@ -68,7 +78,7 @@ resource "aws_s3_bucket_website_configuration" "website" {
   }
 }
 
-# Optional: Bucket policy to allow public read (for static site hosting)
+# Bucket policy to allow public read (for static site hosting)
 resource "aws_s3_bucket_policy" "static_site_policy" {
   bucket = aws_s3_bucket.static_site.id
 
@@ -84,3 +94,115 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
     ]
   })
 }
+
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM Policy for Lambda to access S3 and CloudWatch Logs
+resource "aws_iam_policy" "lambda_s3_policy" {
+  name        = "lambda_s3_policy"
+  description = "Allow Lambda to access S3 bucket and write logs"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket",
+          "s3:DeleteObject"
+        ],
+        Resource = [
+          "${aws_s3_bucket.static_site.arn}",
+          "${aws_s3_bucket.static_site.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# IAM Policy Attachment for basic Lambda execution
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# IAM Policy Attachment for S3 access
+resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_s3_policy.arn
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "lambdaCode.py" 
+  output_path = "lambdaCode.zip"
+}
+
+
+# Lambda Function
+resource "aws_lambda_function" "s3_triggered_lambda" {
+  function_name = "s3_trigger_lambda"
+
+  filename = data.archive_file.lambda.output_path
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+  
+  handler       = "lambdaCode.lambda_handler"  # Specify the correct handler
+  runtime       = "python3.11"
+  role          = aws_iam_role.lambda_exec_role.arn
+  timeout       = 60
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic_execution,
+    aws_iam_role_policy_attachment.lambda_s3_policy_attachment
+  ]
+}
+
+# Permission for S3 to invoke Lambda
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_triggered_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.static_site.arn
+}
+
+# S3 Notification to trigger Lambda
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.static_site.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s3_triggered_lambda.arn
+    events              = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
+}
+
+
